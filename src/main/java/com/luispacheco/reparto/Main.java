7747c783d97e93c4042ae6e3b3b682c0e6100c8f
dev luis pacheco <lapr271090@gmail.com>
2026-06-14 17:31:09 +0200

    Crear DistanciaService con formula Haversine y probar con Madrid-Barcelona

--- src/main/java/com/luispacheco/reparto/Main.java
+++ src/main/java/com/luispacheco/reparto/Main.java
@@ -1,7 +1,13 @@
 package com.luispacheco.reparto;
 
+import com.luispacheco.reparto.service.distanciaService;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Reparto Router - fase 1 iniciada");
+
+        distanciaService distanciaService = new distanciaService();
+        double distancia = distanciaService.calcularDistanciaKm(40.4168, -3.7038, 41.3851, 2.1734);
+        System.out.println("Distancia Madrid-Barcelona: " + distancia + " km");
     }
 }
